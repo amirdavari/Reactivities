c2e6c3ed74f27c83e9a5d825bc297cefa7837967
amirdavari <amir.davari@gmx.de>
2019-12-25 17:41:13 +0100

    CQRS

---
+++ Persistence/Migrations/20191225114421_AddDescriptionToActivity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddDescriptionToActivity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Activities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Activities");
+        }
+    }
+}
